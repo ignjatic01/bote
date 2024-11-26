6f559e0c59b536b7627200161f3062af12d369fd
ignjatic01 <mica.ignjatic01@gmail.com>
2024-11-26 18:56:04 +0100

    Game window data Binding

---
+++ BOTE/BOTE/Util/ArrayIndexConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace BOTE.Util
+{
+    class ArrayIndexConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter is string parameterString)
+            {
+                // Parse parameter string '0,0' into two integers
+                var indices = parameterString.Split(',');
+                int row = int.Parse(indices[0]);
+                int column = int.Parse(indices[1]);
+
+                // Assuming value is a 2D array (BattlefieldUnits)
+                int[,] array = value as int[,];
+                if (array != null)
+                {
+                    return array[row, column];
+                }
+            }
+
+            return DependencyProperty.UnsetValue; // or some default value
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BOTE/BOTE/Views/Game.xaml.cs
+++ BOTE/BOTE/Views/Game.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BOTE.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,10 +20,72 @@ namespace BOTE.Views
     /// </summary>
     public partial class Game : Window
     {
+        public string imagePath = "pack://application:,,,/Images/";
+        public string baldwinName = "Baldwin IV";
+        public string dusanName = "Tsar Dušan";
+        public string saladinName = "Salah ad-Din";
+        public string nevskyName = "Nevsky";
+
+        //Fields in possession
+        public int numOfFields1 { get; set; } = 4;
+        public int numOfFields2 { get; set; } = 5;
+
+        //Player 1 units
+        public int numOfInfantry1 { get; set; } = 0;
+        public int numOfCavalry1 { get; set; } = 0;
+        public int numOfCannons1 { get; set; } = 0;
+
+        //Player 1 units
+        public int numOfInfantry2 { get; set; } = 0;
+        public int numOfCavalry2 { get; set; } = 0;
+        public int numOfCannons2 { get; set; } = 0;
+
+        //Battlefield units
+        public int[,] BattlefieldUnits { get; set; } = new int[9, 3];
+
+        public string Player1ImageSourcePath { get; set; }
+        public string Player2ImageSourcePath { get; set; }
         public Game()
         {
-            InitializeComponent();
+            Player player1 = (Player)Application.Current.Resources["Player1"];
+            Player player2 = (Player)Application.Current.Resources["Player2"];
+
+            if (player1.Name == baldwinName)
+            {
+                Player1ImageSourcePath = imagePath + "baldwin.png";
+            }
+            else if (player1.Name.Equals(dusanName))
+            {
+                Player1ImageSourcePath = imagePath + "dusan.png";
+            }
+            else if (player1.Name == saladinName)
+            {
+                Player1ImageSourcePath = imagePath + "saladin.png";
+            }
+            else if(player1.Name == nevskyName)
+            {
+                Player1ImageSourcePath = imagePath + "nevsky.png";
+            }
+
+            if (player2.Name == baldwinName)
+            {
+                Player2ImageSourcePath = imagePath + "baldwin.png";
+            }
+            else if (player2.Name == dusanName)
+            {
+                Player2ImageSourcePath = imagePath + "dusan.png";
+            }
+            else if (player2.Name == saladinName)
+            {
+                Player2ImageSourcePath = imagePath + "saladin.png";
+            }
+            else if (player2.Name == nevskyName)
+            {
+                Player2ImageSourcePath = imagePath + "nevsky.png";
+            }
             
+            DataContext = this;
+            InitializeComponent();
         }
     }
 }
