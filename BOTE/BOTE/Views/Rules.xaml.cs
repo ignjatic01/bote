1e2e883fdb4dd7bea75ce46f3d76c8e601fb3a3b
ignjatic01 <mica.ignjatic01@gmail.com>
2024-12-02 22:30:13 +0100

    Reading Rules from txt file

--- BOTE/BOTE/Views/Rules.xaml.cs
+++ BOTE/BOTE/Views/Rules.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,11 +18,33 @@ namespace BOTE.Views
     /// <summary>
     /// Interaction logic for Rules.xaml
     /// </summary>
+
     public partial class Rules : Window
     {
+        public string Rule1 { get; set; } = "";
+        public string Rule2 { get; set; } = "";
+        public string Rule3 { get; set; } = "";
+        public string Rule4 { get; set; } = "";
+        public string FilePath = "Data/rules.txt";
         public Rules()
         {
             InitializeComponent();
+            string[] lines;
+            try
+            {
+                string fullPath = System.IO.Path.GetFullPath(FilePath);
+                lines = File.ReadAllLines(fullPath);
+                Rule1 = lines[0];
+                Rule2 = lines[1];
+                Rule3 = lines[2];
+                Rule4 = lines[3];
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            DataContext = this;
         }
     }
 }
